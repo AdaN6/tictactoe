817bdccd230610aa94d27324a273acded82d21f2
AdaN6 <a_disney_da@hotmail.com>
2023-04-24 22:34:52 +0200

    created title and player name section

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 
 import { ref, computed } from 'vue';
 
-const player = ref('x');
+const player = ref('X');
 const board = ref([
   ['', '', ''],
   ['', '', ''],
@@ -56,9 +56,15 @@ const ResetGame = () => {
 </script>
 
 <template>
-  <h1 class="text-3xl font-bold underline">
-    hello World
-  </h1>
+  
+  <main class="pt-8 text-center ">
+    <h1 class="mb-8 text-3xl font-bold uppercase">
+      Tic Tac Toe
+    </h1>
+    <h3 class="text-xl mb-4">
+      Player {{ player }}'s turn'
+    </h3>
+  </main>
     
 </template>
 
